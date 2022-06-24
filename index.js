347abfade1da887c588ccc1538cfdefd4bee8722
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-06-24 06:10:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let myName = "Onyemma Gregory Somtochukwu.\n";
+let height = 5;
+let country = "Nigeria.\n";
+
+alert("My name is " + myName + "I am " + height + " feet tall.\n" + "I am from " + country + "Thank you!");
